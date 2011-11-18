e99c92784a9f10f79adf12eb8f0e11b3970aea49
Adam Kalsey <adam@kalsey.com>
2011-11-17 20:23:27 -0800

    Initial checkin

---
+++ templates/login.tpl.php
@@ -0,0 +1,14 @@
+<div id="luxe-wrap">
+  <div id="luxe-login">
+    <h1><?php print $login_title; ?></h1>
+    <?php print $login_form; ?>
+  </div>
+
+  <div id="luxe-login-register">
+    <h1><?php print $login_register_title; ?></h1>
+    <p>
+      <?php print $login_register_intro; ?>
+    </p>
+    <?php print $login_register_form; ?>
+  </div>
+</div>
